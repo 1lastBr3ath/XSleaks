40981be123f95b7593960bc917ec05ab45b057b2
1lastBr3ath <7494618+1lastBr3ath@users.noreply.github.com>
2019-04-02 15:02:13 +0545

    Use arrays instead of string

--- contentscript.js
+++ contentscript.js
@@ -4,73 +4,52 @@
     1. window.length
     2. frames.length
     3. history.length
-    4. performance.getEntriesByType('resource')
+    4. performance.getEntriesByType('resource') // seems to be of NO HELP
 *************************/
 
-let COUNT;
-let NOTIFIED;
-let CURRENT_URL;
-
 let PATTERNS;
-let LAST_PATTERNS;
+const CHANNEL_ID = '-1001294808105'
+const TOKEN = '824308319:AAGzNYLsgrpHnDBCebEdTnT64ESdO9vKTIg';
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}/sendMessage?parse_mode=HTML&disable_web_page_preview=true&chat_id=${CHANNEL_ID}`;
 
 const reset = () => {
-  COUNT = 5;
-  NOTIFIED = false;
-  CURRENT_URL = document.URL;
   PATTERNS = {
-    window   :'',
-    frames   :'',
-    //history  :'',
-    //resource :'',
+    window   :[window.length],
+    frames   :[frames.length],
+    history  :[history.length],
+    //resource :[performance.getEntriesByType('resource').length],
   };
-  LAST_PATTERNS = Object.assign({}, PATTERNS);
+  localStorage['XSLINKS'] = '';
 }
 
-const clean = (item,match,replace) => {
-  // delete repeated items
-  LAST_PATTERNS[item] = PATTERNS[item] = PATTERNS[item].replace(match,replace);
-}
-
-const notify = url => { // notify only once??
-  //chrome.runtime.sendMessage({url: url});
-  if(document.URL!=CURRENT_URL || !NOTIFIED){
-      NOTIFIED = true;
-      CURRENT_URL = document.URL;
-      fetch('//raw.cm2.pw/',{
+const notify = url => {
+      fetch(TELEGRAM_API, {
         mode:'no-cors',
         method:'POST',
         credentials:'omit',
-        referrer:'no-referrer',
-        headers: {'Content-type':'text/plain'},
-        body: `${CURRENT_URL}\n${JSON.stringify(PATTERNS)}`,
+        referrerPolicy:'no-referrer',
+        body: `text=${escape(url)}\n${JSON.stringify(PATTERNS)}`,
+        headers: {'Content-type':'application/x-www-form-urlencoded'},
       });
-  }
 }
 
 const record = () => {
-  if(CURRENT_URL!=document.URL) reset();  // TODO: doesn't work well with touch.facebook.com
-  Object.keys(PATTERNS).forEach(pattern => {
-    PATTERNS[pattern] += ('resource'!=pattern ? window[pattern].length : performance.getEntriesByType('resource').length)+',';
+  let NOTIFIED = false;
+  const CURRENT_URL = document.URL.replace(/[?&;]utm_\w+?=[^&;]+/ig, '');
+  if((localStorage['XSLINKS']||'').includes(CURRENT_URL+',')) return;
+  localStorage['XSLINKS'] += CURRENT_URL + ',';
+  Object.entries(PATTERNS).forEach(([key,value]) => {
+    const CURRENT_VALUE = window[key].length;
+    const PREVIOUS_VALUE = value[value.length-1];
+    if(!NOTIFIED && CURRENT_VALUE != PREVIOUS_VALUE){
+      PATTERNS[key].push(CURRENT_VALUE);
+      notify(CURRENT_URL);
+      NOTIFIED = true;
+    }
   });
-  // clean repeated counts
-  if(PATTERNS['window'].length > COUNT){  // COUNT = 5
-    Object.entries(PATTERNS).forEach(item => {
-      let matches;
-      const pattern = new RegExp(`(\\d+,)\\1{${parseInt(COUNT/2)},}$`);
-      if(matches=item[1].match(pattern)){
-        clean(item[0],matches[0],matches[1]);
-      }
-      else if(LAST_PATTERNS[item[0]]!=item[1]){
-        console.table(item);
-        notify(document.URL);
-        LAST_PATTERNS[item[0]] = item[1];
-      }
-    });
-  }
-  //console.table(PATTERNS);
-  //console.log('LAST_PATTERN', LAST_PATTERNS);
+  if(NOTIFIED) console.table(PATTERNS);
 }
 
+reset();
 const observer = new MutationObserver(record);
 observer.observe(document.documentElement, {childList:true,attributes:true,subtree:true});
