c0bc48936f43c911341b81dc6b0ae14b26f75ef7
1lastBr3ath <7494618+1lastBr3ath@users.noreply.github.com>
2019-04-05 19:40:35 +0545

    Add setInterval, for better reliability

--- contentscript.js
+++ contentscript.js
@@ -7,7 +7,9 @@
     4. performance.getEntriesByType('resource') // seems to be of NO HELP
 *************************/
 
+let TIMER;
 let PATTERNS;
+let NOTIFIED;
 const CHANNEL_ID = '-281857647';
 const TOKEN = '824308319:AAGzNYLsgrpHnDBCebEdTnT64ESdO9vKTIg';
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}/sendMessage?parse_mode=HTML&disable_web_page_preview=true&chat_id=${CHANNEL_ID}`;
@@ -19,10 +21,15 @@ const reset = () => {
     //history  :[history.length],
     //resource :[performance.getEntriesByType('resource').length],
   };
-  localStorage['XSLINKS'] = localStorage['XSLINKS']||'';
+  NOTIFIED = false;
+  console.table(PATTERNS);
 }
 
-const notify = url => {
+const notify = (url, clear=false) => {
+      if(clear){
+        clearInterval(TIMER);
+        if(Object.values(PATTERNS).filter(i=>i.length>1)==false) return;
+      }
       fetch(TELEGRAM_API, {
         mode:'no-cors',
         method:'POST',
@@ -34,23 +41,35 @@ const notify = url => {
 }
 
 const record = () => {
-  let NOTIFIED = false;
-  const SAVED_URLS = localStorage['XSLINKS'] || '';
-  const CURRENT_URL = document.URL.replace(/[?&;]utm_\w+?=[^&;]+/ig, '');
-  if(SAVED_URLS.includes(CURRENT_URL+'\n')) return;
-  localStorage['XSLINKS'] = SAVED_URLS + CURRENT_URL + '\n';
   Object.entries(PATTERNS).forEach(([key,value]) => {
     const CURRENT_VALUE = window[key].length;
     const PREVIOUS_VALUE = value[value.length-1];
-    if(!NOTIFIED && CURRENT_VALUE != PREVIOUS_VALUE){
+    if(CURRENT_VALUE != PREVIOUS_VALUE){
       PATTERNS[key].push(CURRENT_VALUE);
-      notify(CURRENT_URL);
-      NOTIFIED = true;
+      if(!NOTIFIED){
+        NOTIFIED = true;
+        notify(document.URL.replace(/[?&;]utm_\w+?=[^&;]+/ig, ''));
+      }
+      console.table(PATTERNS);
     }
   });
-  if(NOTIFIED) console.table(PATTERNS);
 }
 
 reset();
+TIMER = setInterval(record, 100);
+setTimeout(notify, 1000 * 30, document.URL, TIMER);
 const observer = new MutationObserver(record);
 observer.observe(document.documentElement, {childList:true,attributes:true,subtree:true});
+
+/***** From @terjanq *****/
+/*
+ var lastLength = -1;
+    var start = Date.now();
+    var int = setInterval(()=>{
+        if(window.length != lastLength){
+            lastLength = window.length;
+            console.log(lastLength, Date.now() - start);
+        }
+    },100);
+  setTimeout(clearInterval, 10000, int);
+*/
